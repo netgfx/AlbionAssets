11e0d966a7ce568f09c2dbc27b39359be23a8e16
m.dobekidis <m.dobekidis@7linternational.com>
2020-05-12 10:26:32 +0300

    added basic functionality and ability to create account if one doesn't exist

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const fs = require('fs');
 var serveIndex = require('serve-index');
 var serveStatic = require('serve-static');
 const del = require('del');
+var CryptoJS = require("crypto-js");
 // encryption
-const algorithm = 'aes-256-cbc';
-const iv = crypto.randomBytes(16);
+
 
 const app = express()
 const port = 8000
@@ -61,8 +61,8 @@ bot.on("message", (message) => {
 
     if (message.content === "ping") {
         message.channel.send("pong");
-    } else if (message.content === "-help") {
-        message.channel.send("```Available Commands:\n-help <Help list>\n-ping <test>\nprive <initates PM between you and the bot>\nstore location tab-name <stores the attachment images securely and assigns them to location-tab-name>```");
+    } else if (message.content === "assets-help") {
+        message.channel.send("```Available Commands:\nassets-help <Help list>\nping <test>\nprive <initates PM between you and the bot>\nadd location tab-name password(optional) <stores the attachment images securely and assigns them to location-tab-name>\nshow location tab-name password(optional) <Retrieves all saved screens from server>\nupload <to upload the last attachments>```");
     } else if (message.content === "prive") {
         if (message.channel.type === "dm") {
             // simply respond
@@ -74,10 +74,13 @@ bot.on("message", (message) => {
     } else if (message.content.indexOf("add") !== -1) {
         var location = message.content.split(" ")[1].toLowerCase() || "caerleon";
         var tab = message.content.split(" ")[2].toLowerCase() || "bank";
+        var password = message.content.split(" ")[3];
+
         let authorId = { "username": message.author.username, "id": message.author.id };
         if (cachedAttachements[authorId.username] === undefined) {
             readDatabaseSnapshot(message.author.username, function() {
-                addAttachments(message, location, tab);
+
+                addAttachments(message, location, tab, password);
             });
         } else {
             let attachments = [];
@@ -85,8 +88,17 @@ bot.on("message", (message) => {
             _.forEach(message.attachments.array(), o => {
                 console.log(location, tab, cachedAttachements);
                 console.log(cachedAttachements[authorId.username][location][tab].length);
+
+                // check if user has provided a password //
+                if (password !== undefined) {
+                    let encrypted = encrypt(o.attachment, password);
+                    console.log("ENCRYPTED: ", encrypted);
+                    cachedAttachements[authorId.username][location][tab].push(encrypted);
+                } else {
+                    cachedAttachements[authorId.username][location][tab].push(o.attachment);
+                }
                 attachments.push(JSON.stringify(o));
-                cachedAttachements[authorId.username][location][tab].push(o.attachment);
+
             });
 
             console.log("on add: ", cachedAttachements);
@@ -95,30 +107,59 @@ bot.on("message", (message) => {
     } else if (message.content.indexOf("show") !== -1) {
         let location = message.content.split(" ")[1].toLowerCase() || "caerleon";
         let tab = message.content.split(" ")[2].toLowerCase() || "bank";
-
+        var password = message.content.split(" ")[3];
+        message.channel.startTyping();
         readDatabaseSnapshot(message.author.username, function() {
             let obj = cachedAttachements[message.author.username];
-            let locationResult = obj[location][tab];
-            //const attachment = new Discord.MessageAttachment(obj.attachment);
-            // Send the attachment in the message channel
-            // message.channel.send(obj.location + " - " + obj.tab, attachment);
-            console.log("SHOWING: ", location, tab, locationResult);
-            const messageEmbed = new Discord.MessageEmbed().setTitle("Contents of: " + location + " - " + tab).attachFiles(locationResult).setTimestamp().setFooter('Made by Netgfx');
+            var locationResult = obj[location][tab];
 
-            message.channel.send(messageEmbed).catch(error => {
-                console.log(error);
-            });
+            console.log("length of result: ", locationResult, locationResult.length, location, tab);
+            if (locationResult.length === 1) {
+                if (password !== undefined) {
+                    locationResult = decrypt(locationResult[0], password);
+                }
+                console.log("SHOWING: ", location, tab, locationResult);
+                const messageEmbed = new Discord.MessageEmbed().setTitle("Contents of: " + location + " - " + tab).attachFiles(locationResult).setTimestamp().setFooter('Made by Netgfx');
+
+                message.channel.send(messageEmbed).catch(error => {
+                    console.log(error);
+                    message.channel.stopTyping();
+                }).then(result => {
+                    message.channel.stopTyping();
+                });
+            } else if (locationResult.length > 1) {
+                let messageEmbed;
+                _.forEach(locationResult, o => {
+                    var _image = o;
+                    if (password !== undefined) {
+                        _image = decrypt(_image, password);
+                    }
+                    messageEmbed = new Discord.MessageEmbed().setTitle("Contents of: " + location + " - " + tab).attachFiles(_image).setTimestamp().setFooter('Made by Netgfx');
+
+                    message.channel.send(messageEmbed).catch(error => {
+                        console.log(error);
+                        message.channel.stopTyping();
+                    }).then(result => {
+                        message.channel.stopTyping();
+                    });
+                });
+            } else if (locationResult.length === 0) {
+                message.channel.send("No data found for this city or location");
+                message.channel.stopTyping();
+            }
 
         });
 
 
 
     } else if (message.content.indexOf("upload") !== -1) {
-        let password = message.content.split(" ")[1];
-        console.log("encrypting with password: ", password); // duh remove this //..
 
         console.log(cachedAttachements);
-        uploadUserData(cachedAttachements[message.author.username], message.author.username, password);
+        message.channel.startTyping();
+        uploadUserData(cachedAttachements[message.author.username], message.author.username, () => {
+            message.channel.send("Assets uploaded successfully");
+            message.channel.stopTyping();
+        });
     }
 
 });
@@ -128,9 +169,9 @@ bot.on("message", (message) => {
  *
  * @param {*} message
  */
-function addAttachments(message, location, tab) {
+function addAttachments(message, location, tab, password) {
 
-    console.log(message);
+    console.log("adding attachment: ", location, tab, password);
     let authorId = { "username": message.author.username, "id": message.author.id };
     let attachments = [];
     //cachedAttachements[authorId.username][location][tab] = [];
@@ -149,7 +190,15 @@ function addAttachments(message, location, tab) {
     _.forEach(message.attachments.array(), o => {
         console.log(cachedAttachements[authorId.username][location][tab].length);
         attachments.push(JSON.stringify(o));
-        cachedAttachements[authorId.username][location][tab].push(o.attachment);
+
+        // check if user has provided a password //
+        if (password !== undefined) {
+            let encrypted = encrypt(o.attachment, password);
+            console.log("ENCRYPTED: ", encrypted);
+            cachedAttachements[authorId.username][location][tab].push(encrypted);
+        } else {
+            cachedAttachements[authorId.username][location][tab].push(o.attachment);
+        }
     });
 
     console.log("on add: ", cachedAttachements);
@@ -201,15 +250,44 @@ function readDatabaseSnapshot(username, callback) {
     return firebase.database().ref('accounts/' + username + "/").once('value').then(function(snapshot) {
         //var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
         let data = snapshot.val();
-        //console.log(data.assets);
-        if (cachedAttachements[username] === undefined) {
-            cachedAttachements[username] = {};
-        }
-        cachedAttachements[username] = data.assets;
-        console.log(">>>> ", cachedAttachements[username]);
+        console.log(data);
 
-        if (callback) {
-            callback();
+        if (data === null) {
+            // account doesn't exist so we must create it first //
+            createAccount(username, () => {
+                // recursive action //
+                readDatabaseSnapshot(username, callback);
+            });
+        } else {
+            if (cachedAttachements[username] === undefined) {
+                cachedAttachements[username] = {};
+            }
+            cachedAttachements[username] = data.assets;
+            console.log(">>>> ", cachedAttachements[username]);
+
+            if (callback) {
+                callback();
+            }
+        }
+    }).catch(error => {
+        console.log(error);
+        _message.channel.stopTyping();
+    });
+}
+
+function createAccount(username, callback) {
+    var obj = {};
+    obj[username] = { "assets": { "stub": "" } };
+
+    firebase.database().ref('accounts/').update(obj, function(error) {
+        if (error) {
+            console.log("error ", error);
+            _message.channel.send("ERROR creating account ", error.message);
+        } else {
+            console.log("account created");
+            if (callback) {
+                callback();
+            }
         }
     });
 }
@@ -222,7 +300,7 @@ function readDatabaseSnapshot(username, callback) {
  * @param {*} location
  * @param {*} tab
  */
-function uploadUserData(encryptedString, username, password) {
+function uploadUserData(encryptedString, username, callback) {
 
     var obj = {};
     obj = encryptedString;
@@ -232,9 +310,14 @@ function uploadUserData(encryptedString, username, password) {
     firebase.database().ref('accounts/' + username + "/assets/").update(obj, function(error) {
         if (error) {
             console.log("error ", error);
+            _message.channel.stopTyping();
+            _message.channel.send("There was an error uploading your assets: ", error.message);
         } else {
             console.log("assets saved!");
             readDatabaseSnapshot(username);
+            if (callback) {
+                callback();
+            }
         }
     });
 }
@@ -248,10 +331,9 @@ function uploadUserData(encryptedString, username, password) {
  * @returns
  */
 function encrypt(text, pass) {
-    let cipher = crypto.createCipheriv(algorithm, pass, iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+    var ciphertext = CryptoJS.AES.encrypt(text, pass).toString();
+
+    return ciphertext;
 }
 
 /**
@@ -262,12 +344,13 @@ function encrypt(text, pass) {
  * @returns
  */
 function decrypt(text, pass) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
-    let decipher = crypto.createDecipheriv(algorithm, pass, iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
+
+    console.log("decrypting: ", text, pass);
+    var bytes = CryptoJS.AES.decrypt(text, pass);
+    var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+    console.log("decrypted: ", originalText);
+    return originalText;
 }
 
 
